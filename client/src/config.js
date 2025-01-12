c0368273a09eba1bcb7d91a6dacfcb0932a25b09
AbhinavVihan <abhinavchaudhary348@gmail.com>
2025-01-12 21:59:59 +0530

    added docker k8s and ci/cd

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 export const api = {  
-  tagsEndPoint: 'http://localhost:5000/tags/',
-  usersEndPoint: 'http://localhost:5000/users/',
-  postsEndPoint: 'http://localhost:5000/posts/',
-  repliesEndPoint: 'http://localhost:5000/reply/',
+  tagsEndPoint: 'http://a25b90b2befa444dd8c8f5acfd55f374-1058877027.us-west-2.elb.amazonaws.com:5000/tags/',
+  usersEndPoint: 'http://a25b90b2befa444dd8c8f5acfd55f374-1058877027.us-west-2.elb.amazonaws.com:5000/users/',
+  postsEndPoint: 'http://a25b90b2befa444dd8c8f5acfd55f374-1058877027.us-west-2.elb.amazonaws.com:5000/posts/',
+  repliesEndPoint: 'http://a25b90b2befa444dd8c8f5acfd55f374-1058877027.us-west-2.elb.amazonaws.com:5000/reply/',
 }
